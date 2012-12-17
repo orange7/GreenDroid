6fbafd46e3c3abf114d91033575b5a7e8293b47f
orange7 <536776960@qq.com>
2012-12-17 16:31:31 +0800

    删除文件 添加文件

--- GreenDroid/src/greendroid/app/ActionBarActivity.java
+++ GreenDroid/src/greendroid/app/ActionBarActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Cyril Mottier (http://www.cyrilmottier.com)
+ * Copyright (C) 2010 Cyril Mottier (http://www.cyrilmottier.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
